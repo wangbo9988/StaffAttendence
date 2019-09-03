36b96d5021684105f86be9f4b4e68b304b9cf423
Mr. Wang <wangbo@wangbodeMacBook-Air.local>
2019-09-03 13:14:34 +0800

    Initial commit

---
+++ templates/static/js/main.js
@@ -0,0 +1,96 @@
+//清除密码修改 span 提示文字
+function resetPassword () {
+    document.getElementById("span_password").innerHTML='';
+    document.getElementById("span_newPassword_1").innerHTML='';
+    document.getElementById("span_newPassword_2").innerHTML='';
+};
+//清除密码修改 input 输入框
+function resetPasswordInput () {
+    document.getElementById("password").value='';
+    document.getElementById("newPassword_1").value='';
+    document.getElementById("newPassword_2").value='';
+};
+//隐藏修改密码模态框时清除span
+$('#myModal').on('hide.bs.modal', function () {
+    resetPassword();
+    resetPasswordInput();
+})
+//修改密码执行表单验证
+function changePassword() {
+    resetPassword();
+    var oldPassword = document.getElementById('password').value;
+    var newPassword_1 = document.getElementById('newPassword_1').value;
+    var newPassword_2 = document.getElementById('newPassword_2').value;
+    if (oldPassword == ""){
+        document.getElementById("span_password").innerHTML='请输入当前密码';
+        return false;
+    }else if (newPassword_1 == ""){
+        document.getElementById("span_newPassword_1").innerHTML='请输入密码';
+        return false;
+    }else if (newPassword_1.length < 8){
+        document.getElementById("span_newPassword_1").innerHTML='密码不可少于8位';
+        return false;
+    }else if (newPassword_2 == ""){
+        document.getElementById("span_newPassword_2").innerHTML='请确认密码';
+        return false;
+    }else if (newPassword_1 != newPassword_2){
+        document.getElementById("span_newPassword_2").innerHTML='两次密码不一致';
+        return false;
+    }
+    //获取表单数据
+    var passwordFormJson = $('#passwordForm').serializeArray();
+    //构造请求参数
+    var passwordSubmitJson = {
+        currencyPassword: passwordFormJson[0].value,
+        newPassword: passwordFormJson[1].value
+    };
+    $.ajax({
+        catch: false,
+        type: "POST",
+        url: "/attend/insert",
+        data: passwordSubmitJson,
+        async: false,
+        success: function (data) {
+            if (data == "SUCCESS") {
+                //成功
+                message('success', '打卡成功');
+            } else if(data == "fail"){
+                //失败
+                message('error', '打卡失败');
+            }else {
+                //其他错误信息
+                message('error', 'data');
+            }
+        }
+    });
+}
+function resetMessage(){
+    /* 全局定义变量 message */
+    result = 'success';
+    message = '登陆成功';
+}
+function message(result,message){
+    /* 全局定义变量 message */
+    if(result != '' && message != ''){
+        if(result == 'success'){
+            resetMessage();
+            alertify.success(message);
+            result = '';
+            message = ''
+        }else if(result == 'error'){
+            resetMessage();
+            alertify.error(message);
+            result = '';
+            message = ''
+        }
+    }
+};
+function dateFormat(time) {
+    /*
+     * 时间格式化
+     * 转换前格式：2017-10-10 08:08
+     * 转换后：2017-10-10T08:08
+     * */
+    time = time.replace(' ','T');
+    return time;
+};
